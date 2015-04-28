e37238cfe531d16c58a2925caa7d036f5874ec75
mondaugen <nicholas.esterer@gmail.com>
2015-04-28 17:51:53 -0400

    Divergence no longer NaN after the addition of a small regularization term to prevent the input matrix from being 0 in NMF. Algorithm seems to work OK, but is slow. Added a script to make plots of spectrograms at various window and hop sizes.

--- scripts/nmf.m
+++ scripts/nmf.m
@@ -1,22 +1,86 @@
-function [B,G] = nmf(X,J,N=100,verbose=1)
+function [B,G] = nmf(X,J,N=100,verbose=1,sanity=0)
 % Factorizes X of size K-by-T into the matrices B and G each of size K-by-J and
 % J-by-T resp.
 % N is the number of iterations to perform.
 % verbose - if non-zero, algorithm prints out current difference between X and
 % B*G as the sum of the squared differences of each element, divided by the
 % number of elements in X 
+% sanity - if non-zero, algorithm checks values of matrices
 [K,T] = size(X);
 B = rand(K,J);
 G = rand(J,T);
+X += 1.0E-10; % Add regularization
 %R=sum(sum(X))/sum(sum(B*G));
 %B=sqrt(R)*B;
 %G=sqrt(R)*G;
 ONES = ones(K,T);
 for n=(1:N),
-    B = B.*((X./(B*G))*G')./(ONES*G');
-    G = G.*(B'*(X./(B*G)))./(B'*ONES);
+    % Do updates and add regularization
+    B = B.*((X./(B*G))*G')./(ONES*G') + 1.0E-10;
+    G = G.*(B'*(X./(B*G)))./(B'*ONES) + 1.0E-10;
+%    G(1:J,1:T) = G(1:J,1:T).*sum(B(:,(1:J)).*X(:,(1:T))./(B*G))./sum(B(:,(1:J)));
+%    B = B.*sum(G.*X./(B*G))./sum(G);
     if verbose && (mod(n,verbose) == 0),
-        div = sum(sum(X.*log(X./(B*G)).-X.+(B*G)))/numel(X);
+        TMP=B*G;
+        if sanity,
+            fprintf(stderr,'X negative? ');
+            if (any(any(X < 0))),
+                fprintf(stderr,'Yes');
+            else,
+                fprintf(stderr,'No');
+            end;
+            fprintf(stderr,'.\n');
+            fprintf(stderr,'B*G negative? ');
+            if (any(any(TMP < 0))),
+                fprintf(stderr,'Yes');
+            else,
+                fprintf(stderr,'No');
+            end;
+            fprintf(stderr,'.\n');
+            fprintf(stderr,'B*G == 0? ');
+            if (any(any(TMP == 0))),
+                fprintf(stderr,'Yes');
+            else,
+                fprintf(stderr,'No');
+            end;
+            fprintf(stderr,'.\n');
+            fprintf(stderr,'X == 0? ');
+            if (any(any(X == 0))),
+                fprintf(stderr,'Yes');
+            else,
+                fprintf(stderr,'No');
+            end;
+            fprintf(stderr,'.\n');
+            fprintf(stderr,'X == NaN? ');
+            if (any(any(X == NaN))),
+                fprintf(stderr,'Yes');
+            else,
+                fprintf(stderr,'No');
+            end;
+            fprintf(stderr,'.\n');
+            fprintf(stderr,'B*G == NaN? ');
+            if (any(any(TMP == NaN))),
+                fprintf(stderr,'Yes');
+            else,
+                fprintf(stderr,'No');
+            end;
+            fprintf(stderr,'.\n');
+            fprintf(stderr,'X == +-Inf? ');
+            if (any(any(X == -Inf)) || any(any(X == Inf))),
+                fprintf(stderr,'Yes');
+            else,
+                fprintf(stderr,'No');
+            end;
+            fprintf(stderr,'.\n');
+            fprintf(stderr,'B*G == +-Inf? ');
+            if (any(any(TMP == -Inf)) || any(any(TMP == Inf))),
+                fprintf(stderr,'Yes');
+            else,
+                fprintf(stderr,'No');
+            end;
+            fprintf(stderr,'.\n');
+        end;
+        div = sum(sum(X.*log(X./(TMP)).-X.+(TMP)))/numel(X);
         fprintf(stderr,'iter: %d, diff: %f\n',n,div);
     end
 end;
---
+++ scripts/voice_sg_plots.m
@@ -0,0 +1,36 @@
+#!/usr/bin/octave -qf
+% Writes the long and short plots of the spectrogram
+if (nargin != 3),
+    error('Missing arguments: path N_fft, Hop');
+end
+pkg load signal
+args=argv();
+[path, N_fft,Hop]=args{:};
+N_fft = str2num(N_fft);
+Hop = str2num(Hop);
+[x,fs]=wavread(path);
+[S,F,T]=specgram(x,N_fft,fs,N_fft,N_fft-Hop);
+handle=imagesc(flipud(log10(abs(S)/10)));
+L=size(S,2);
+ax=gca;
+set(ax,'ytick',[0:N_fft/8:N_fft/2]);
+set(ax,'yticklabel', ...
+    cellfun(@num2str, ...
+        num2cell(round((N_fft/2-get(ax,'ytick'))/N_fft*fs)), ...
+        'UniformOutput',false));
+set(ax,'xtick',[0:L/4:L]);
+set(ax,'xticklabel', ...
+    cellfun(@num2str, ...
+        num2cell(get(ax,'xtick')*Hop/44100), ...
+        'UniformOutput',false));
+%figure(1);
+axis("square");
+savpath=sprintf('%s_%d_%d.%s',cell2mat( ...
+        ostrsplit( cell2mat(ostrsplit(path,'/')(end)), '.')(1)),
+        N_fft,Hop,'eps');
+ylabel('Frequency (Hz)');
+xlabel('Time (s)');
+title( ...
+    sprintf('Log Magnitude Spectrogram\n(Window Size: %d Hop Size: %d)', ...
+        N_fft,Hop));
+print(savpath,'-depsc2');
