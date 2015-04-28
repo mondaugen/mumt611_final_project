46148d264b6475ddd3e24cccb4a309418e80d177
mondaugen <nicholas.esterer@gmail.com>
2015-04-27 22:00:35 -0400

    Debugging the scripts. For some reason, the divergence is always NaN when using small window sizes.

--- scripts/dis_measure.m
+++ scripts/dis_measure.m
@@ -1,4 +1,4 @@
 function [d] = dis_measure(x)
 % DIS_MEASURE -- Find the discontinuity measure of signal x.
 x=x(:);
-d=sum(diff(x).^2)/sum(x);
+d=sum((x(2:end)-x(1:end-1)).^2)/sum(x.^2);
--- scripts/nmf.m
+++ scripts/nmf.m
@@ -1,12 +1,22 @@
-function [B,G] = nmf(X,J,N=100)
+function [B,G] = nmf(X,J,N=100,verbose=1)
 % Factorizes X of size K-by-T into the matrices B and G each of size K-by-J and
 % J-by-T resp.
 % N is the number of iterations to perform.
+% verbose - if non-zero, algorithm prints out current difference between X and
+% B*G as the sum of the squared differences of each element, divided by the
+% number of elements in X 
 [K,T] = size(X);
 B = rand(K,J);
 G = rand(J,T);
+%R=sum(sum(X))/sum(sum(B*G));
+%B=sqrt(R)*B;
+%G=sqrt(R)*G;
 ONES = ones(K,T);
 for n=(1:N),
     B = B.*((X./(B*G))*G')./(ONES*G');
     G = G.*(B'*(X./(B*G)))./(B'*ONES);
+    if verbose && (mod(n,verbose) == 0),
+        div = sum(sum(X.*log(X./(B*G)).-X.+(B*G)))/numel(X);
+        fprintf(stderr,'iter: %d, diff: %f\n',n,div);
+    end
 end;
